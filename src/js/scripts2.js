7f05229ecc5349a800934050e08bbe4ac372c7f7
Felipe Augusto <felipe.silva@medroom.com.br>
2018-09-11 19:39:34 -0300

    em desenv

--- src/js/scripts2.js
+++ src/js/scripts2.js
@@ -1,6 +1,5 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'phaser-example', { preload: preload, create: create, update: update, render: render });
 
-
 /*
     virtual gamecontroller with phaser.js
     buttons, sprite(sheet)s (animation) and code of this example by valueerror - mario by nintendo ;-)
@@ -21,6 +20,8 @@ var jump = false;
 var keyMusic = "keymusic";
 var music;
 var sounds;
+var stars;
+var platforms;
 
 function preload() {
 
@@ -29,6 +30,7 @@ function preload() {
 
     //background, ground, fireball images
     game.load.image('ground', 'assets/misc/2048x48-ground.png');
+    game.load.image('platform', 'assets/platform.png');
     game.load.image('clouds', 'assets/misc/clouds.jpg');
     game.load.image('fireball', 'assets/misc/fireball.png', 40, 30);
     game.load.image('sky', 'assets/sky.png');
@@ -71,39 +73,124 @@ function start() {
     music.loopFull(0.6);
 }
 
+/**
+ * Criação de grupo de estrelas
+ */
+function createStars() {
+    stars = game.physics.add.group({
+        key: 'star',
+        repeat: 11,
+        setXY: { x: 12, y: 0, stepX: 70 }
+    });
+
+    //Ao colidir estrelas com plataformas, é realizado calculo para que
+    //a estrela gradualmente vá se aproximando do plataforma, até que fique
+    //totalmente parada sobre a plataforma
+    stars.children.iterate(function (child) {
+        child.setBounceY(Phaser.Math.FloatBetween(0.4, 0.8));
+    });
+}
+
+/**
+ * Cria imagens na cena
+ */
+function createImages() {
+    game.add.image(400, 300, 'sky');
+    game.add.image(400, 300, 'star');
+}
+
+/**
+ * Cria as plataformas na cena
+ */
+function createPlatforms() {
+
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //  A simple background for our game
+    game.add.sprite(0, 0, 'sky');
+
+    //  The platforms group contains the ground and the 2 ledges we can jump on
+    platforms = game.add.group();
+
+    //  We will enable physics for any object that is created in this group
+    platforms.enableBody = true;
+
+    // Here we create the ground.
+    var ground = platforms.create(0, game.world.height - 64, 'platform');
+
+    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    ground.scale.setTo(2, 2);
+
+    //  This stops it from falling away when you jump on it
+    ground.body.immovable = true;
+
+    //  Now let's create two ledges
+    var ledge = platforms.create(400, 400, 'platform');
+
+    ledge.body.immovable = true;
+
+    ledge = platforms.create(-150, 250, 'platform');
+
+    ledge.body.immovable = true;
+}
+
+/**
+ * Executa a música de fundo
+ */
+function playMusic() {
+    music = game.add.audio('music');
+    sounds = [music];
+    game.sound.setDecodedCallback(sounds, start, this);
+}
+
 function create() {
     if (!game.device.desktop) { game.input.onDown.add(gofull, this); } //go fullscreen on mobile devices
 
-    music = game.add.audio('music');
-    sounds = [ music ];    
-    game.sound.setDecodedCallback(sounds, start, this);
+    playMusic();
 
+    //createImages();
+
+    //createStars();
+
+    createPlatforms();
+    
     game.physics.startSystem(Phaser.Physics.P2JS);  //activate physics
     game.physics.p2.gravity.y = 1200;  //realistic gravity
-    game.world.setBounds(0, 0, 2000, 600);//(x, y, width, height)
+    //game.world.setBounds(0, 0, 2000, 600);//(x, y, width, height)
     game.physics.p2.setBoundsToWorld(true, true, false, true, false); //(left, right, top, bottom, setCollisionGroup)
     game.physics.p2.friction = 5;   // default friction between ground and player or fireballs
 
-    clouds = game.add.tileSprite(0, 0, 2048, 600, 'clouds'); //add tiling sprite to cover the whole game world
-    ground = game.add.sprite(game.world.width / 2, game.world.height - 24, 'ground');
-    game.physics.p2.enable(ground);  //enable physics so our player will not fall through ground but collide with it
-    ground.body.static = true;    // ground should not move
+    //clouds = game.add.tileSprite(0, 0, 2048, 600, 'clouds'); //add tiling sprite to cover the whole game world
+    //ground = game.add.sprite(game.world.width / 2, game.world.height - 24, 'ground');
+    //game.physics.p2.enable(ground);  //enable physics so our player will not fall through ground but collide with it
+    //ground.body.static = true;    // ground should not move
 
     fireballs = game.add.group();  // add a new group for fireballs
     fireballs.createMultiple(500, 'fireball', 0, false);  // create plenty of them hidden and out of the game world
 
     //setup our player
-    player = game.add.sprite(350, game.world.height - 150, 'mario'); //create and position player
+    player = game.add.sprite(350, game.world.height - 150, 'mario'); //create and position player    
     game.physics.p2.enable(player);
     player.body.setCircle(22);  // collision circle 
     player.body.fixedRotation = true; // do not rotate on collision
     player.body.mass = 4;
+    
+
+     //game.physics.arcade.enable(player); 
+     //player.body.bounce.y = 0.2;
+     //player.body.gravity.y = 300;
+     //player.body.collideWorldBounds = true;
 
     // add some animations 
     player.animations.add('walk', [1, 2, 3, 4], 10, true);  // (key, framesarray, fps,repeat)
     player.animations.add('duck', [11], 0, true);
     player.animations.add('duckwalk', [10, 11, 12], 3, true);
-    game.camera.follow(player); //always center player
+    //game.camera.follow(player); //always center player
+
+    // add some animations 
+    // player.animations.add('left', [0, 1, 2, 3], 10, true);
+    // player.animations.add('right', [5, 6, 7, 8], 10, true);
 
     // create our virtual game controller buttons 
     buttonjump = game.add.button(600, 500, 'buttonjump', null, this, 0, 1, 0, 1);  //game, x, y, key, callback, callbackContext, overFrame, outFrame, downFrame, upFrame
@@ -136,6 +223,12 @@ function create() {
 };
 
 function update() {
+
+     //  Collide the player and the stars with the platforms
+     //var hitPlatform = game.physics.arcade.collide(player, platforms);     
+
+     //game.physics.p2.collide(player, platforms);
+
     // define what should happen when a button is pressed
     if (left && !duck) {
         player.scale.x = -1;
@@ -173,7 +266,6 @@ function update() {
 function render() {
     game.debug.text('jump:' + jump + ' duck:' + duck + ' left:' + left + ' right:' + right + ' fire:' + fire, 20, 20);
 }
-
 
 //some useful functions
 function gofull() { game.scale.startFullScreen(false); }
