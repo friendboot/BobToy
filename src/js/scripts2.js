0ac2a552d2b8f020fe03f434ecd7146e7fb4d804
Felipe Augusto <felipe.silva@medroom.com.br>
2018-09-11 21:43:27 -0300

    Inserido estrelas e captura delas

--- src/js/scripts2.js
+++ src/js/scripts2.js
@@ -12,6 +12,9 @@ var music;
 var sounds;
 var stars;
 var platforms;
+var score = 0;
+var scoreText;
+var bombs;
 
 function preload() {
 
@@ -66,9 +69,6 @@ function start() {
  */
 function createPlatforms() {
 
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-    
     //  A simple background for our game
     game.add.sprite(0, 0, 'sky');
 
@@ -106,17 +106,69 @@ function playMusic() {
     game.sound.setDecodedCallback(sounds, start, this);
 }
 
+function createStars() {
+    stars = game.add.group();
+
+    stars.enableBody = true;
+
+    //  Here we'll create 12 of them evenly spaced apart
+    for (var i = 0; i < 12; i++) {
+        //  Create a star inside of the 'stars' group
+        var star = stars.create(i * 70, 0, 'star');
+
+        //  Let gravity do its thing
+        star.body.gravity.y = 400;
+
+        //  This just gives each star a slightly random bounce value
+        star.body.bounce.y = 0.7 + Math.random() * 0.2;
+    }
+}
+
+/** 
+ * Criação de grupo de bombas que poderão colidir com a plataforma e o player
+*/
+// function createBombs() {
+
+//     bombs = game.add.group();
+//     bombs.enableBody = true;
+
+//     var bomb = bombs.create(i * 70, 0, 'bomb');
+//     bomb.body.gravity.y = 400;
+//     bomb.body.bounce.y = 0.7 + Math.random() * 0.2;
+
+//     game.physics.add.collider(bombs, platforms);
+
+//     game.physics.add.collider(player, bombs, hitBomb, null, this);
+// }
+
+/**
+ * Criação de objeto que representa o texto de score apresentado no canto
+ * superior esquerdo
+ */
+function createScoreText() {
+    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
+}
+
 function create() {
     if (!game.device.desktop) { game.input.onDown.add(gofull, this); } //go fullscreen on mobile devices
 
-    playMusic();
+    //  We're going to be using physics, so enable the Arcade Physics system
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    //playMusic();
+
+    createPlatforms();
+
+    createStars();
+
+    createScoreText();
+
+    //createBombs();
 
     //createImages();
 
     //createStars();
 
-    createPlatforms();
-    
     //game.physics.arcade.gravity.y = 1200;  //realistic gravity
     //game.world.setBounds(0, 0, 2000, 600);//(x, y, width, height)
     game.physics.arcade.setBoundsToWorld(true, true, false, true, false); //(left, right, top, bottom, setCollisionGroup)
@@ -128,7 +180,7 @@ function create() {
     //ground.body.static = true;    // ground should not move
 
     //setup our player
-    player = game.add.sprite(350, game.world.height - 150, 'mario'); //create and position player    
+    player = game.add.sprite(450, game.world.height - 150, 'mario'); //create and position player    
     game.physics.arcade.enable(player);
     player.body.setCircle(22);  // collision circle 
     player.body.fixedRotation = true; // do not rotate on collision
@@ -170,6 +222,8 @@ function update() {
 
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms);
+    game.physics.arcade.collide(stars, platforms);
+    game.physics.arcade.overlap(player, stars, collectStar, null, this);
 
     //  Reset the players velocity (movement)
     player.body.velocity.x = 0;
@@ -211,10 +265,58 @@ function update() {
 function render() {
 }
 
+/**
+ * Caso o player encoste em uma das bombas
+ * @param {*} player 
+ * Objeto que representa o player que será manipulado pel usuário
+ * @param {*} bomb 
+ * Objeto que representa a bomba encostada pelo player
+ */
+function hitBomb(player, bomb) {
+    game.physics.pause();
+
+    player.setTint(0xff0000);
+
+    player.anims.play('turn');
+
+    music.stop();
+
+    let musicBomb = this.sound.add('bomb');
+    musicBomb.play();
+
+    let musicGameOver = this.sound.add('gameover');
+    musicGameOver.play();
+}
+
+function collectStar(player, star) {
+
+    // Removes the star from the screen
+    star.kill();
+
+    //  Add and update the score
+    score += 10;
+    scoreText.text = 'Score: ' + score;
+
+    //if (stars.countActive(true) === 0) {
+
+    //    stars.children.iterate(function (child) {
+    //        child.enableBody(true, child.x, 0, true, true);
+    //    });
+
+    //    var x = (player.x < 400) ? Phaser.Math.Between(400, 800) : Phaser.Math.Between(0, 400);
+
+    //    var bomb = bombs.create(x, 16, 'bomb');
+    //    bomb.setBounce(1);
+    //    bomb.setCollideWorldBounds(true);
+    //    bomb.setVelocity(Phaser.Math.Between(-200, 200), 20);
+    //    bomb.allowGravity = false;
+    //}
+}
+
 //some useful functions
 function gofull() { game.scale.startFullScreen(false); }
 function jump_now() {  //jump with small delay
     if (player.body.touching.down) {
-        player.body.velocity.y = -350;        
+        player.body.velocity.y = -350;
     }
 }
